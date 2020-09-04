b851c3fa8ced1cceaa17e2c30aa010de41b35063
Drew Taylor <drew@infiniteinternet.ca>
2020-09-04 13:59:51 -0400

    refactor mineral creation, add random spawn positions

--- Mining/src/game.ts
+++ Mining/src/game.ts
@@ -1,17 +1,12 @@
-import utils from '../node_modules/decentraland-ecs-utils/index';
-import { InterpolationType } from '../node_modules/decentraland-ecs-utils/transform/math/interpolation';
-import * as ui from '../node_modules/@dcl/ui-utils/index';
+// import utils from '../node_modules/decentraland-ecs-utils/index';
+// import { InterpolationType } from '../node_modules/decentraland-ecs-utils/transform/math/interpolation';
+// import * as ui from '../node_modules/@dcl/ui-utils/index';
 
-// Params
-let inventory = [];
-let sphaleriteIcon;
-let galenaIcon;
-let pyriteIcon;
-let zinciteIcon;
-let chalcopyriteIcon;
-let hopeiteIcon;
-let ghaniteIcon;
-let amethystIcon;
+// import { ImageSection } from '../node_modules/@dcl/ui-utils/utils/types';
+
+import { Mineral } from "./mineral";
+
+// Mineral types
 const SPHALERITE = "sphalerite";
 const GALENA = "galena";
 const PYRITE = "pyrite";
@@ -20,676 +15,111 @@ const CHALCOPYRITE = "chalcopyrite";
 const HOPEITE = "hopeite";
 const GHANITE = "ghanite";
 const AMETHYST = "amethyst";
-let isMiningSphalerite = false;
-let isMiningGalena = false;
-let isMiningPyrite = false;
-let isMiningZincite = false;
-let isMiningChalcopyrite = false;
-let isMiningHopeite = false;
-let isMiningGhanite = false;
-let isMiningAmethyst = false;
 
-// Sounds
-// Mining sound effect
-const diggingSound = new Entity();
-diggingSound.addComponent(
-  new AudioSource(
-    new AudioClip('sounds/shovel.mp3')
-  )
-);
-diggingSound.addComponent(new Transform());
-diggingSound.getComponent(Transform).position = Camera.instance.position;
-engine.addEntity(diggingSound);
+// Sprite sheet positions (inventory icons)
+const spritePositions = {};
+spritePositions[SPHALERITE] = {
+  sourceWidth: 50, 
+  sourceHeight: 50, 
+  sourceLeft: 480
+};
+spritePositions[GALENA] = {
+  sourceWidth: 50, 
+  sourceHeight: 50, 
+  sourceLeft: 336, 
+  sourceTop: 95
+};
+spritePositions[PYRITE] = {
+  sourceWidth: 50, 
+  sourceHeight: 50, 
+  sourceLeft: 432
+};
+spritePositions[ZINCITE] = {
+  sourceWidth: 50, 
+  sourceHeight: 50, 
+  sourceLeft: 144, 
+  sourceTop: 95
+};
+spritePositions[CHALCOPYRITE] = {
+  sourceWidth: 50, 
+  sourceHeight: 50, 
+  sourceLeft: 192,  
+  sourceTop: 95
+};
+spritePositions[HOPEITE] = {
+  sourceWidth: 50, 
+  sourceHeight: 50, 
+  sourceLeft: 192
+};
+spritePositions[GHANITE] = {
+  sourceWidth: 50, 
+  sourceHeight: 50, 
+  sourceLeft: 336
+};
+spritePositions[AMETHYST] = {
+  sourceWidth: 50, 
+  sourceHeight: 50, 
+  sourceLeft: 240, 
+  sourceTop: 95
+};
 
+// 3D Models
+const models = {};
+models[SPHALERITE] = new GLTFShape('models/minerals/sphalerite.glb');
+models[GALENA] = new GLTFShape('models/minerals/galena.glb');
+models[PYRITE] = new GLTFShape('models/minerals/pyrite.glb');
+models[ZINCITE] = new GLTFShape('models/minerals/zincite.glb');
+models[CHALCOPYRITE] = new GLTFShape('models/minerals/chalcopyrite.glb');
+models[HOPEITE] = new GLTFShape('models/minerals/hopeite.glb');
+models[GHANITE] = new GLTFShape('models/minerals/ghanite.glb');
+models[AMETHYST] = new GLTFShape('models/minerals/amethyst.glb');
+
+// Create minerals
 // Sphalerite (y)
-const sphalerite = new Entity();
-sphalerite.addComponent(new GLTFShape('models/minerals/sphalerite.glb'));
-sphalerite.addComponent(new Transform({
-  position: new Vector3(7,0,7)
-}));
-
-engine.addEntity(sphalerite);
-
-sphalerite.addComponent(
-  new OnPointerDown(
-    (e) => {
-      if (inventory.length < 3) {
-        // Play digging sound
-        diggingSound.getComponent(AudioSource).playOnce();
-        // Reduce or remove pile ore deposit
-        miningEvent(SPHALERITE);
-      } else {
-        ui.displayAnnouncement('You\'re carrying a heavy load');
-      }
-    },
-    { 
-      button: ActionButton.POINTER, 
-      hoverText: 'Sphalerite Ore' 
-    }
-  )
+const sphalerite = new Mineral(
+  models[SPHALERITE],
+  SPHALERITE,
+  spritePositions[SPHALERITE]
 );
-
 // Galena (n)
-const galena = new Entity();
-galena.addComponent(new GLTFShape('models/minerals/galena.glb'));
-galena.addComponent(new Transform({
-  position: new Vector3(3,0,3)
-}));
-
-engine.addEntity(galena);
-
-galena.addComponent(
-  new OnPointerDown(
-    (e) => {
-      if (inventory.length < 3) {
-        // Play digging sound
-        diggingSound.getComponent(AudioSource).playOnce();
-        // Reduce or remove pile ore deposit
-        miningEvent(GALENA);
-      } else {
-        ui.displayAnnouncement('You\'re carrying a heavy load');
-      }
-    },
-    { 
-      button: ActionButton.POINTER, 
-      hoverText: 'Galena Ore' 
-    }
-  )
+const galena = new Mineral(
+  models[GALENA],
+  GALENA,
+  spritePositions[GALENA]
 );
-
 // Pyrite (n)
-const pyrite = new Entity();
-pyrite.addComponent(new GLTFShape('models/minerals/pyrite.glb'));
-pyrite.addComponent(new Transform({
-  position: new Vector3(2,0,9)
-}));
-
-engine.addEntity(pyrite);
-
-pyrite.addComponent(
-  new OnPointerDown(
-    (e) => {
-      if (inventory.length < 3) {
-        // Play digging sound
-        diggingSound.getComponent(AudioSource).playOnce();
-        // Reduce or remove pile ore deposit
-        miningEvent(PYRITE);
-      } else {
-        ui.displayAnnouncement('You\'re carrying a heavy load');
-      }
-    },
-    { 
-      button: ActionButton.POINTER, 
-      hoverText: 'Pyrite Ore' 
-    }
-  )
+const pyrite = new Mineral(
+  models[PYRITE],
+  PYRITE,
+  spritePositions[PYRITE]
 );
-
 // Zincite (n)
-const zincite = new Entity();
-zincite.addComponent(new GLTFShape('models/minerals/zincite.glb'));
-zincite.addComponent(new Transform({
-  position: new Vector3(6,0,2)
-}));
-
-engine.addEntity(zincite);
-
-zincite.addComponent(
-  new OnPointerDown(
-    (e) => {
-      if (inventory.length < 3) {
-        // Play digging sound
-        diggingSound.getComponent(AudioSource).playOnce();
-        // Reduce or remove pile ore deposit
-        miningEvent(ZINCITE);
-      } else {
-        ui.displayAnnouncement('You\'re carrying a heavy load');
-      }
-    },
-    { 
-      button: ActionButton.POINTER, 
-      hoverText: 'Zincite Crystal' 
-    }
-  )
+const zincite = new Mineral(
+  models[ZINCITE],
+  ZINCITE,
+  spritePositions[ZINCITE]
 );
-
 // Chalcopyrite (n)
-const chalcopyrite = new Entity();
-chalcopyrite.addComponent(new GLTFShape('models/minerals/chalcopyrite.glb'));
-chalcopyrite.addComponent(new Transform({
-  position: new Vector3(14,0,14)
-}));
-
-engine.addEntity(chalcopyrite);
-
-chalcopyrite.addComponent(
-  new OnPointerDown(
-    (e) => {
-      if (inventory.length < 3) {
-        // Play digging sound
-        diggingSound.getComponent(AudioSource).playOnce();
-        // Reduce or remove pile ore deposit
-        miningEvent(CHALCOPYRITE);
-      } else {
-        ui.displayAnnouncement('You\'re carrying a heavy load');
-      }
-    },
-    { 
-      button: ActionButton.POINTER, 
-      hoverText: 'Chalcopyrite Ore' 
-    }
-  )
+const chalcopyrite = new Mineral(
+  models[CHALCOPYRITE],
+  CHALCOPYRITE,
+  spritePositions[CHALCOPYRITE]
 );
-
 // Hopeite (m)
-const hopeite = new Entity();
-hopeite.addComponent(new GLTFShape('models/minerals/hopeite.glb'));
-hopeite.addComponent(new Transform({
-  position: new Vector3(11,0,12)
-}));
-
-engine.addEntity(hopeite);
-
-hopeite.addComponent(
-  new OnPointerDown(
-    (e) => {
-      if (inventory.length < 3) {
-        // Play digging sound
-        diggingSound.getComponent(AudioSource).playOnce();
-        // Reduce or remove pile ore deposit
-        miningEvent(HOPEITE);
-      } else {
-        ui.displayAnnouncement('You\'re carrying a heavy load');
-      }
-    },
-    { 
-      button: ActionButton.POINTER, 
-      hoverText: 'Hopeite Ore' 
-    }
-  )
+const hopeite = new Mineral(
+  models[HOPEITE],
+  HOPEITE,
+  spritePositions[HOPEITE]
 );
-
 // Ghanite (y)
-const ghanite = new Entity();
-ghanite.addComponent(new GLTFShape('models/minerals/ghanite.glb'));
-ghanite.addComponent(new Transform({
-  position: new Vector3(13,0,9)
-}));
-
-engine.addEntity(ghanite);
-
-ghanite.addComponent(
-  new OnPointerDown(
-    (e) => {
-      if (inventory.length < 3) {
-        // Play digging sound
-        diggingSound.getComponent(AudioSource).playOnce();
-        // Reduce or remove pile ore deposit
-        miningEvent(GHANITE);
-      } else {
-        ui.displayAnnouncement('You\'re carrying a heavy load');
-      }
-    },
-    { 
-      button: ActionButton.POINTER, 
-      hoverText: 'Ghanite Ore' 
-    }
-  )
+const ghanite = new Mineral(
+  models[GHANITE],
+  GHANITE,
+  spritePositions[GHANITE]
 );
-
 // Amethyst (n)
-const amethyst = new Entity();
-amethyst.addComponent(new GLTFShape('models/minerals/amethyst.glb'));
-amethyst.addComponent(new Transform({
-  position: new Vector3(11,0,8)
-}));
-
-engine.addEntity(amethyst);
-
-amethyst.addComponent(
-  new OnPointerDown(
-    (e) => {
-      if (inventory.length < 3) {
-        // Play digging sound
-        diggingSound.getComponent(AudioSource).playOnce();
-        // Reduce or remove pile ore deposit
-        miningEvent(AMETHYST);
-      } else {
-        ui.displayAnnouncement('You\'re carrying a heavy load');
-      }
-    },
-    { 
-      button: ActionButton.POINTER, 
-      hoverText: 'Amethyst Ore' 
-    }
-  )
+const amethyst = new Mineral(
+  models[AMETHYST],
+  AMETHYST,
+  spritePositions[AMETHYST]
 );
-
-function miningEvent(type: string) {
-  switch (type) {
-    case SPHALERITE:
-      sphaleriteMiningEvent();
-      break;
-    case GALENA:
-      galenaMiningEvent();
-      break;
-    case PYRITE:
-      pyriteMiningEvent();
-      break;
-    case ZINCITE:
-      zinciteMiningEvent();
-      break;
-    case CHALCOPYRITE:
-      chalcopyriteMiningEvent();
-      break;
-    case HOPEITE:
-      hopeiteMiningEvent();
-      break;
-    case GHANITE:
-      ghaniteMiningEvent();
-      break;
-    case AMETHYST:
-      amethystMiningEvent();
-      break;
-  }
-}
-
-function sphaleriteMiningEvent() {
-  if (isMiningSphalerite) {
-    engine.removeEntity(sphalerite);
-    let xOffset = 0;
-    switch (inventory.length) {
-      case 0:
-        xOffset = -25;
-        break;
-      case 1:
-        xOffset = -80;
-        break;
-      case 2:
-        xOffset = -135;
-        break;
-    }
-
-    // Handle Inventory
-    sphaleriteIcon = new ui.SmallIcon(
-      'models/icons/ores.png', 
-      // x, y
-      xOffset, 80, 
-      // Width, height
-      48, 48, 
-      // Sprite sheet position
-      {sourceWidth: 50, sourceHeight: 50, sourceLeft: 480}
-    );
-    
-    // Add to inventory
-    inventory.push(SPHALERITE);
-
-    ui.displayAnnouncement('Sphalerite added to your inventory');
-
-  } else {
-    isMiningSphalerite = true;
-    let currentScale = sphalerite.getComponent(Transform).position.z;
-    let startScale = new Vector3(currentScale * 0.05, currentScale * 0.05, currentScale * 0.05);
-    let endScale = new Vector3(currentScale * 0.025, currentScale * 0.025, currentScale * 0.025);
-
-    sphalerite.addComponent(
-      new utils.ScaleTransformComponent(
-        startScale,
-        endScale,
-        0.5,
-        null,
-        InterpolationType.LINEAR
-      )
-    );
-  }
-};
-
-function galenaMiningEvent() {
-  if (isMiningGalena) {
-    engine.removeEntity(galena);
-    let xOffset = 0;
-    switch (inventory.length) {
-      case 0:
-        xOffset = -25;
-        break;
-      case 1:
-        xOffset = -80;
-        break;
-      case 2:
-        xOffset = -135;
-        break;
-    }
-
-    // Handle Inventory
-    galenaIcon = new ui.SmallIcon(
-      'models/icons/ores.png', 
-      // x, y
-      xOffset, 80, 
-      // Width, height
-      48, 48, 
-      // Sprite sheet position
-      {sourceWidth: 50, sourceHeight: 50, sourceLeft: 336, sourceTop: 95}
-    );
-    
-    // Add to inventory
-    inventory.push(GALENA);
-
-    ui.displayAnnouncement('Galena added to your inventory');
-
-  } else {
-    isMiningGalena = true;
-    let currentScale = galena.getComponent(Transform).position.z;
-    let startScale = new Vector3(currentScale * 0.05, currentScale * 0.05, currentScale * 0.05);
-    let endScale = new Vector3(currentScale * 0.025, currentScale * 0.025, currentScale * 0.025);
-
-    galena.addComponent(
-      new utils.ScaleTransformComponent(
-        startScale,
-        endScale,
-        0.5,
-        null,
-        InterpolationType.LINEAR
-      )
-    );
-  }
-};
-
-function pyriteMiningEvent() {
-  if (isMiningPyrite) {
-    engine.removeEntity(pyrite);
-    let xOffset = 0;
-    switch (inventory.length) {
-      case 0:
-        xOffset = -25;
-        break;
-      case 1:
-        xOffset = -80;
-        break;
-      case 2:
-        xOffset = -135;
-        break;
-    }
-
-    // Handle Inventory
-    pyriteIcon = new ui.SmallIcon(
-      'models/icons/ores.png', 
-      // x, y
-      xOffset, 80, 
-      // Width, height
-      48, 48, 
-      // Sprite sheet position
-      {sourceWidth: 50, sourceHeight: 50, sourceLeft: 432}
-    );
-    
-    // Add to inventory
-    inventory.push(PYRITE);
-
-    ui.displayAnnouncement('Pyrite added to your inventory');
-
-  } else {
-    isMiningPyrite = true;
-    let currentScale = pyrite.getComponent(Transform).position.z;
-    let startScale = new Vector3(currentScale * 0.05, currentScale * 0.05, currentScale * 0.05);
-    let endScale = new Vector3(currentScale * 0.025, currentScale * 0.025, currentScale * 0.025);
-
-    pyrite.addComponent(
-      new utils.ScaleTransformComponent(
-        startScale,
-        endScale,
-        0.5,
-        null,
-        InterpolationType.LINEAR
-      )
-    );
-  }
-};
-
-function zinciteMiningEvent() {
-  if (isMiningZincite) {
-    engine.removeEntity(zincite);
-    let xOffset = 0;
-    switch (inventory.length) {
-      case 0:
-        xOffset = -25;
-        break;
-      case 1:
-        xOffset = -80;
-        break;
-      case 2:
-        xOffset = -135;
-        break;
-    }
-
-    // Handle Inventory
-    zinciteIcon = new ui.SmallIcon(
-      'models/icons/ores.png', 
-      // x, y
-      xOffset, 80, 
-      // Width, height
-      48, 48, 
-      // Sprite sheet position
-      {sourceWidth: 50, sourceHeight: 50, sourceLeft: 144, sourceTop: 95}
-    );
-    
-    // Add to inventory
-    inventory.push(ZINCITE);
-
-    ui.displayAnnouncement('Zincite added to your inventory');
-
-  } else {
-    isMiningZincite = true;
-    let currentScale = zincite.getComponent(Transform).position.z;
-    let startScale = new Vector3(currentScale * 0.05, currentScale * 0.05, currentScale * 0.05);
-    let endScale = new Vector3(currentScale * 0.025, currentScale * 0.025, currentScale * 0.025);
-
-    zincite.addComponent(
-      new utils.ScaleTransformComponent(
-        startScale,
-        endScale,
-        0.5,
-        null,
-        InterpolationType.LINEAR
-      )
-    );
-  }
-};
-
-function chalcopyriteMiningEvent() {
-  if (isMiningChalcopyrite) {
-    engine.removeEntity(chalcopyrite);
-    let xOffset = 0;
-    switch (inventory.length) {
-      case 0:
-        xOffset = -25;
-        break;
-      case 1:
-        xOffset = -80;
-        break;
-      case 2:
-        xOffset = -135;
-        break;
-    }
-
-    // Handle Inventory
-    chalcopyriteIcon = new ui.SmallIcon(
-      'models/icons/ores.png', 
-      // x, y
-      xOffset, 80, 
-      // Width, height
-      48, 48, 
-      // Sprite sheet position
-      {sourceWidth: 50, sourceHeight: 50, sourceLeft: 192,  sourceTop: 95}
-    );
-    
-    // Add to inventory
-    inventory.push(CHALCOPYRITE);
-
-    ui.displayAnnouncement('Chalcopyrite added to your inventory');
-
-  } else {
-    isMiningChalcopyrite = true;
-    let currentScale = chalcopyrite.getComponent(Transform).position.z;
-    let startScale = new Vector3(currentScale * 0.05, currentScale * 0.05, currentScale * 0.05);
-    let endScale = new Vector3(currentScale * 0.025, currentScale * 0.025, currentScale * 0.025);
-
-    chalcopyrite.addComponent(
-      new utils.ScaleTransformComponent(
-        startScale,
-        endScale,
-        0.5,
-        null,
-        InterpolationType.LINEAR
-      )
-    );
-  }
-};
-
-function hopeiteMiningEvent() {
-  if (isMiningHopeite) {
-    engine.removeEntity(hopeite);
-    let xOffset = 0;
-    switch (inventory.length) {
-      case 0:
-        xOffset = -25;
-        break;
-      case 1:
-        xOffset = -80;
-        break;
-      case 2:
-        xOffset = -135;
-        break;
-    }
-
-    // Handle Inventory
-    hopeiteIcon = new ui.SmallIcon(
-      'models/icons/ores.png', 
-      // x, y
-      xOffset, 80, 
-      // Width, height
-      48, 48, 
-      // Sprite sheet position
-      {sourceWidth: 50, sourceHeight: 50, sourceLeft: 192}
-    );
-    
-    // Add to inventory
-    inventory.push(HOPEITE);
-
-    ui.displayAnnouncement('Hopeite added to your inventory');
-
-  } else {
-    isMiningHopeite = true;
-    let currentScale = hopeite.getComponent(Transform).position.z;
-    let startScale = new Vector3(currentScale * 0.05, currentScale * 0.05, currentScale * 0.05);
-    let endScale = new Vector3(currentScale * 0.025, currentScale * 0.025, currentScale * 0.025);
-
-    hopeite.addComponent(
-      new utils.ScaleTransformComponent(
-        startScale,
-        endScale,
-        0.5,
-        null,
-        InterpolationType.LINEAR
-      )
-    );
-  }
-};
-
-function ghaniteMiningEvent() {
-  if (isMiningGhanite) {
-    engine.removeEntity(ghanite);
-    let xOffset = 0;
-    switch (inventory.length) {
-      case 0:
-        xOffset = -25;
-        break;
-      case 1:
-        xOffset = -80;
-        break;
-      case 2:
-        xOffset = -135;
-        break;
-    }
-
-    // Handle Inventory
-    ghaniteIcon = new ui.SmallIcon(
-      'models/icons/ores.png', 
-      // x, y
-      xOffset, 80, 
-      // Width, height
-      48, 48, 
-      // Sprite sheet position
-      {sourceWidth: 50, sourceHeight: 50, sourceLeft: 336}
-    );
-    
-    // Add to inventory
-    inventory.push(GHANITE);
-
-    ui.displayAnnouncement('Ghanite added to your inventory');
-
-  } else {
-    isMiningGhanite = true;
-    let currentScale = ghanite.getComponent(Transform).position.z;
-    let startScale = new Vector3(currentScale * 0.05, currentScale * 0.05, currentScale * 0.05);
-    let endScale = new Vector3(currentScale * 0.025, currentScale * 0.025, currentScale * 0.025);
-
-    ghanite.addComponent(
-      new utils.ScaleTransformComponent(
-        startScale,
-        endScale,
-        0.5,
-        null,
-        InterpolationType.LINEAR
-      )
-    );
-  }
-};
-
-function amethystMiningEvent() {
-  if (isMiningAmethyst) {
-    engine.removeEntity(amethyst);
-    let xOffset = 0;
-    switch (inventory.length) {
-      case 0:
-        xOffset = -25;
-        break;
-      case 1:
-        xOffset = -80;
-        break;
-      case 2:
-        xOffset = -135;
-        break;
-    }
-
-    // Handle Inventory
-    amethystIcon = new ui.SmallIcon(
-      'models/icons/ores.png', 
-      // x, y
-      xOffset, 80, 
-      // Width, height
-      48, 48, 
-      // Sprite sheet position
-      {sourceWidth: 50, sourceHeight: 50, sourceLeft: 240, sourceTop: 95}
-    );
-    
-    // Add to inventory
-    inventory.push(AMETHYST);
-
-    ui.displayAnnouncement('Amethyst added to your inventory');
-
-  } else {
-    isMiningAmethyst = true;
-    let currentScale = amethyst.getComponent(Transform).position.z;
-    let startScale = new Vector3(currentScale * 0.05, currentScale * 0.05, currentScale * 0.05);
-    let endScale = new Vector3(currentScale * 0.025, currentScale * 0.025, currentScale * 0.025);
-
-    amethyst.addComponent(
-      new utils.ScaleTransformComponent(
-        startScale,
-        endScale,
-        0.5,
-        null,
-        InterpolationType.LINEAR
-      )
-    );
-  }
-};
---
+++ Mining/src/mineral.ts
@@ -0,0 +1,179 @@
+import utils from '../node_modules/decentraland-ecs-utils/index';
+import { InterpolationType } from '../node_modules/decentraland-ecs-utils/transform/math/interpolation';
+import * as ui from '../node_modules/@dcl/ui-utils/index';
+import { ImageSection } from '../node_modules/@dcl/ui-utils/utils/types';
+
+// External params
+const SPHALERITE = "sphalerite";
+const GALENA = "galena";
+const PYRITE = "pyrite";
+const ZINCITE = "zincite";
+const CHALCOPYRITE = "chalcopyrite";
+const HOPEITE = "hopeite";
+const GHANITE = "ghanite";
+const AMETHYST = "amethyst";
+let inventory = [];
+let minSpawn = 2;
+let maxSpawn = 14;
+let currentSpawns = {
+  x: [],
+  z: []
+};
+const X_COORD = 0;
+const Y_COORD = 1;
+
+// Sounds
+// Mining sound effect
+const diggingSound = new Entity();
+diggingSound.addComponent(
+  new AudioSource(
+    new AudioClip('sounds/shovel.mp3')
+  )
+);
+diggingSound.addComponent(new Transform());
+diggingSound.getComponent(Transform).position = Camera.instance.position;
+engine.addEntity(diggingSound);
+
+// Mineral
+export class Mineral extends Entity {
+  constructor(
+    // Constructed
+    model: Entity,
+    public mineralType: string,
+    public spritePosition: ImageSection,
+    // Defaulted
+    private isMining: boolean = false,
+    private icon: ui.SmallIcon = null,
+  ) {
+    super();
+
+    this.addComponent(model);
+
+    let position = this.getSpawnPosition();
+    this.addComponent(new Transform({
+      position: new Vector3(position.x, position.y ,position.z)
+    }));
+
+    // XXX TODO: add random model scale size
+
+    engine.addEntity(this);
+
+    this.addComponent(
+      new OnPointerDown(
+        (e) => {
+          if (inventory.length < 3) {
+            // Play digging sound
+            diggingSound.getComponent(AudioSource).playOnce();
+            // Reduce or remove pile ore deposit
+            this.miningEvent(this.mineralType);
+          } else {
+            ui.displayAnnouncement('You\'re carrying a heavy load');
+          }
+        },
+        { 
+          button: ActionButton.POINTER, 
+          hoverText: (this.mineralType == ZINCITE) ? 'Zincite Crystal' : this.mineralType.charAt(0).toUpperCase() + this.mineralType.slice(1) + ' Ore' 
+        }
+      )
+    );
+  }
+
+  private getSpawnPosition() {
+    let randomX = this.randomCoordinate(X_COORD);
+    let randomZ = this.randomCoordinate(Y_COORD);
+
+    let spawnPosition = {
+      x: randomX,
+      y: 0,
+      z: randomZ
+    };
+
+    currentSpawns.x.push(spawnPosition.x);
+    currentSpawns.z.push(spawnPosition.z);
+
+    return spawnPosition;
+  };
+
+  private randomCoordinate(spawnAxis: number = 0) {
+    let randomNum = Math.round(Math.random() * (maxSpawn - minSpawn) + minSpawn);
+    
+    // XXX (note): Um, so this collider protection doesn't really work
+    if (spawnAxis == X_COORD) {
+      if (
+        currentSpawns.x.indexOf(randomNum) !== -1 
+        && currentSpawns.x.indexOf(randomNum + 1) !== -1 
+        && currentSpawns.x.indexOf(randomNum - 1) !== -1 
+        && currentSpawns.x.indexOf(randomNum + 2) !== -1 
+        && currentSpawns.x.indexOf(randomNum - 2) !== -1  
+      ) {
+        randomNum = this.randomCoordinate(X_COORD);
+      }
+    } else {
+      if (
+        currentSpawns.z.indexOf(randomNum) !== -1
+        && currentSpawns.z.indexOf(randomNum + 1) !== -1 
+        && currentSpawns.z.indexOf(randomNum - 1) !== -1 
+        && currentSpawns.z.indexOf(randomNum + 2) !== -1 
+        && currentSpawns.z.indexOf(randomNum - 2) !== -1 
+      ) {
+        randomNum = this.randomCoordinate(Y_COORD);
+      }
+    }
+
+    return randomNum;
+  }
+
+  private miningEvent(type: string) {
+    let ucaseType = type.charAt(0).toUpperCase() + type.slice(1);
+
+    // Handle mining event
+    if (this.isMining) {
+      engine.removeEntity(this);
+      let xOffset = 0;
+      switch (inventory.length) {
+        case 0:
+          xOffset = -25;
+          break;
+        case 1:
+          xOffset = -80;
+          break;
+        case 2:
+          xOffset = -135;
+          break;
+      }
+  
+      // Handle Inventory
+      this.icon = new ui.SmallIcon(
+        'models/icons/ores.png', 
+        // x, y
+        xOffset, 80, 
+        // Width, height
+        48, 48, 
+        // Sprite sheet position
+        this.spritePosition
+      );
+      
+      // Add to inventory
+      inventory.push(type);
+  
+      // Inform player of inventory item
+      ui.displayAnnouncement(ucaseType + ' added to your inventory');
+  
+    } else {
+      this.isMining = true;
+      let currentScale = this.getComponent(Transform).position.z;
+      let startScale = new Vector3(currentScale * 0.05, currentScale * 0.05, currentScale * 0.05);
+      let endScale = new Vector3(currentScale * 0.025, currentScale * 0.025, currentScale * 0.025);
+  
+      this.addComponent(
+        new utils.ScaleTransformComponent(
+          startScale,
+          endScale,
+          0.5,
+          null,
+          InterpolationType.LINEAR
+        )
+      );
+    }
+  };
+};
